476e19798d8c8f973236722d95aeb9681e995d12
DESKTOP-E31NF7D\madeleine <madeleine@mihailides.com>
2017-06-19 17:01:39 +1000

    Fixed skewing bug (caused by not passing in parent rotation and scaling), added boundary for object pick up, and made script applicable to any object.

--- CrazyBones/Assets/Scripts/DropAndAttachArm1.cs
+++ CrazyBones/Assets/Scripts/DropAndAttachArm1.cs
@@ -4,39 +4,62 @@ using System;
 
 public class DropAndAttachArm1 : MonoBehaviour {
 
-	// Check whether arm can be reattached.
+	// Check whether object can be reattached.
 	private bool isAttached = true;
+	private Vector3 originalScale;
+	private Vector3 objectGlobalPos;
+	private Vector3 objectLocalPos;
+	private GameObject player;
 
 	// Use this for initialization
 	void Start () {
-	
+		// Set object scale.
+		originalScale = gameObject.transform.localScale;
+		// Set local position.
+		objectLocalPos = gameObject.transform.localPosition;
+
+		// Obtain parent player object.
+		player = GameObject.Find ("GameObject");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// On key press E drop the arm.
+		// On key press E drop the object.
 		if (Input.GetKeyDown (KeyCode.E)) {
 			if (isAttached == true) {
 				// Set attached variable.
 				isAttached = false;
 
-				// Deattach arm from parent.
+				// Deattach object from parent.
 				gameObject.transform.parent = null;
-				// Set GLOBAL position of arm to it's local x and z position, and global position of 1.
-				gameObject.transform.position = new Vector3(gameObject.transform.localPosition.x, 1, gameObject.transform.localPosition.z);
+				// Set GLOBAL position of object to it's local x and z position, and global position of 1.
+				gameObject.transform.position = new Vector3(gameObject.transform.position.x, 1, gameObject.transform.position.z);
+
+				// Store new object pos.
+				objectGlobalPos = gameObject.transform.position;
 			}
 		}
 		if (Input.GetKeyDown (KeyCode.R)) {
 			if (isAttached == false) {
-				// Attach arm to parent.
-				var player = GameObject.Find("GameObject").transform;
-				gameObject.transform.parent = player;
+				// Check distance between objects position and current parent position.
+				var distance = objectGlobalPos - player.transform.position;
 
-				// Transform position.
-				gameObject.transform.localPosition = new Vector3(0, 0, 3.093745f);
+				// y is set to normal height difference.
+				if (distance.x < 3 && distance.z < 3 && distance.y < 3.093745f) {
+					// Attach object to parent.
+					// Conform rotation and transform to parent, and obtain original scale values for same reason (otherwise object skews).
+					gameObject.transform.parent = player.transform;
+					gameObject.transform.rotation = player.transform.rotation;
+					gameObject.transform.localScale = originalScale;
 
-				// Set attached variable.
-				isAttached = true;
+					// Transform position.
+					gameObject.transform.localPosition = new Vector3 (objectLocalPos.x, objectLocalPos.y, objectLocalPos.z);
+
+					// Set attached variable.
+					isAttached = true;	
+				} else {
+					// Object too far. Print to screen maybe
+				}
 			}
 		}
 	}
