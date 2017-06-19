8df76ce1dfb1be5ce13b764ef3847c3571457ef3
DESKTOP-E31NF7D\madeleine <madeleine@mihailides.com>
2017-06-19 15:49:23 +1000

    Added right arm and drop functionality with e and r - with increased size bug.

---
+++ CrazyBones/Assets/Scripts/DropAndAttachArm1.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DropAndAttachArm1 : MonoBehaviour {
+
+	// Check whether arm can be reattached.
+	private bool isAttached = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// On key press E drop the arm.
+		if (Input.GetKeyDown (KeyCode.E)) {
+			if (isAttached == true) {
+				// Set attached variable.
+				isAttached = false;
+
+				// Deattach arm from parent.
+				gameObject.transform.parent = null;
+				// Set GLOBAL position of arm to it's local x and z position, and global position of 1.
+				gameObject.transform.position = new Vector3(gameObject.transform.localPosition.x, 1, gameObject.transform.localPosition.z);
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.R)) {
+			if (isAttached == false) {
+				// Attach arm to parent.
+				var player = GameObject.Find("GameObject").transform;
+				gameObject.transform.parent = player;
+
+				// Transform position.
+				gameObject.transform.localPosition = new Vector3(0, 0, 3.093745f);
+
+				// Set attached variable.
+				isAttached = true;
+			}
+		}
+	}
+}
